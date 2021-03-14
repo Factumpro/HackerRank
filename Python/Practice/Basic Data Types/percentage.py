01a17db8a78752b53b24e1118e315ceb345daac6
git__repo[login] <git__repo[email]>
2021-03-14 15:30:17 +0000

    practice/percentage.solved

--- Python/Practice/Basic Data Types/percentage.py
+++ Python/Practice/Basic Data Types/percentage.py
@@ -4,24 +4,24 @@ Finding the percentage
   Link to problem statement:
   https://www.hackerrank.com/challenges/finding-the-percentage/problem
 
-The provided code stub will read in a dictionary containing key/value 
-pairs of name:[marks] for a list of students. 
-Print the average of the marks array for the student name provided, 
-showing 2 places after the decimal.
+  The provided code stub will read in a dictionary containing key/value 
+  pairs of name:[marks] for a list of students. 
+  Print the average of the marks array for the student name provided, 
+  showing 2 places after the decimal.
 ''''
 
 '''
 Sample Input 0
 
-3
-Krishna 67 68 69
-Arjun 70 98 63
-Malika 52 56 60
-Malika
+  3
+  Krishna 67 68 69
+  Arjun 70 98 63
+  Malika 52 56 60
+  Malika
 
 Sample Output 0
 
-56.00
+  56.00
 '''
 
 if __name__ == '__main__':
