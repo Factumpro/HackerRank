266d19d1daf23e3d3f17cbe3e1ab2d857a1fc520
Oleg <slidzonaoleg@gmail.com>
2021-03-27 16:29:15 +0100

    Update Day_12.py

--- Python/Tutorials/30 Days of Code/Day_12.py
+++ Python/Tutorials/30 Days of Code/Day_12.py
@@ -1,80 +1,76 @@
+#!/usr/bin/env python3
+
+
 class Person:
-	def __init__(self, firstName, lastName, idNumber):
-		self.firstName = firstName
-		self.lastName = lastName
-		self.idNumber = idNumber
-	def printPerson(self):
-		print("Name:", self.lastName + ",", self.firstName)
-		print("ID:", self.idNumber)
+	"""Abstract class
+	!!! Need documentation written.
+	"""
+	
+    def __init__(self, first_name, last_name, id_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id_number = id_number
+
+    def print_person(self):
+        print("Name: {last_name}, {first_name}"
+              .format(first_name=self.first_name,
+                      last_name=self.last_name))
+        print("ID:", self.id_number)
+
 
 class Student(Person):
-    #   Class Constructor
-    #   
-    #   Parameters:
-    #   firstName - A string denoting the Person's first name.
-    #   lastName - A string denoting the Person's last name.
-    #   id - An integer denoting the Person's ID number.
-    #   scores - An array of integers denoting the Person's test scores.
-    #
-    # Write your constructor here
-    _scores = []
+    """Class Constructor(Inheritance)
+    :first_name - A string denoting the Person's first name.
+    :last_name - A string denoting the Person's last name.
+    :id_number - An integer denoting the Person's ID number.
+    :scores - An array of integers denoting the Person's test scores.
+    Write your constructor here
+    """
 
-    def __init__(self, firstName, lastName, idNumber, scores):
-        super().__init__(firstName, lastName, idNumber)
-        self._scores = scores
-
-    '''
-    def printScore(self):
-        print("scores:", self._scores)
-    '''
+    def __init__(self, first_name, last_name, id_number, scores):
+        super().__init__(first_name, last_name, id_number)
+        self.scores = scores
+        self.scores_ = []
 
     def calculate(self):
-        __average = sum(self._scores)/len(self._scores)
-        if __average >= 70:
-          if __average < 80:
-            return "A"
-          else: 
-            if __average < 90:
-              return "E"
+        self.scores_ = self.scores
+        average_ = sum(self.scores_)/len(self.scores_)
+        if average_ >= 70:
+            if average_ < 80:
+                return "A"
             else:
-              return "O"
+                if average_ < 90:
+                    return "E"
+                else:
+                    return "O"
         else:
-          if __average < 40:
-            return "T"
-          else: 
-            if __average < 55:
-              return "D"
+            if average_ < 40:
+                return "T"
             else:
-              return "P"
-        
-        #print(_sum)       
+                if average_ < 55:
+                    return "D"
+                else:
+                    return "P"
 
-    #   Function Name: calculate
-    #   Return: A character denoting the grade.
-    #
-    # Write your function here
+"""
+####TEST
+    listScore = [ [90,90], [80,80], [70,70], [55,55],[40,40],[35,35] ]
+    for i in listScore:
+      firstName = 'firstName'
+      lastName = 'lastName'
+      idNum = '8135627'
+      scores = i
+      s = Student(first_name, last_name, id_number, scores)
+      s.printPerson()
+      print("Grade:", s.calculate())
+"""
 
-'''
-#TEST
-
-listScore = [ [90,90], [80,80], [70,70], [55,55],[40,40],[35,35] ]
-for i in listScore:
-
-  firstName = 'firstName'
-  lastName = 'lastName'
-  idNum = '8135627'
-  scores = i
-  s = Student(firstName, lastName, idNum, scores)
-  s.printPerson()
-  print("Grade:", s.calculate())
-'''
-
-line = input().split()
-firstName = line[0]
-lastName = line[1]
-idNum = line[2]
-numScores = int(input()) # not needed for Python
-scores = list( map(int, input().split()) )
-s = Student(firstName, lastName, idNum, scores)
-s.printPerson()
-print("Grade:", s.calculate())
+if __name__ == '__main__':
+    first_name,
+    last_name,
+    id_number = input().split()
+    _ = int(input())
+    scores = list(map(int, input().split()))
+    s = Student(first_name, last_name, id_number, scores)
+    s.print_person()
+    print("Grade:", s.calculate())
