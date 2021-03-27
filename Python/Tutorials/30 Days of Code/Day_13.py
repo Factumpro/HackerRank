92d322ed9f335ba8758d18e023a6d856946cddcf
Oleg <slidzonaoleg@gmail.com>
2021-03-27 17:16:40 +0100

    Update Day_13.py

--- Python/Tutorials/30 Days of Code/Day_13.py
+++ Python/Tutorials/30 Days of Code/Day_13.py
@@ -1,26 +1,31 @@
-''''
-Day 13: Abstract Classes
-''''
+#!/usr/bin/env python3
 
-from abc import ABCMeta, abstractmethod
-class Book(object, metaclass=ABCMeta):
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author   
+from abc import ABCMeta
+
+
+class Book(metaclass=ABCMeta):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
     @abstractmethod
-    def display(): pass
+    def display():
+        pass
 
-#Write MyBook class
+
 class MyBook(Book):
     def __init__(self, title, author, price):
-        pass
-            
-    @staticmethod
-    def display():
-        print ('Title: {}\nAuthor: {}\nPrice: {}'.format(title, author, price))
-        
-title=input()
-author=input()
-price=int(input())
-new_novel=MyBook(title,author,price)
-new_novel.display()
+        super(Book, self).__init__()
+        self.price = price
+
+    @classmethod
+    def display(self):
+        print ('Title: {}\nAuthor: {}\nPrice: {}'
+               .format(title, author, price))
+
+if __name__ == '__main__':
+    title = input()
+    author = input()
+    price = int(input())
+    new_novel = MyBook(title, author, price)
+    new_novel.display()
