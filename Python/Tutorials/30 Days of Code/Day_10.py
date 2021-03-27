8f183c57c18df53ff3c54e2eede6c6560223a04d
Oleg <slidzonaoleg@gmail.com>
2021-03-27 14:36:25 +0100

    Update Day_10.py

--- Python/Tutorials/30 Days of Code/Day_10.py
+++ Python/Tutorials/30 Days of Code/Day_10.py
@@ -1,14 +1,35 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
-import math
-import os
-import random
-import re
-import sys
+"""
+    Task
+    ----
+    Given a base-10 integer, Num, convert it to binary (base-2).
+    Then find and print the base-10 integer denoting the maximum number
+    of consecutive 1's in Num's binary representation.
 
+    * to convert in binary, we use {0:b}.format() methods:    
+    >>> "bin: {0:b}".format(5)
+    'bin: 101'
 
+    * then transform in to list without "0" and count all "1" between "0":
+    #5(10) = 101(2)
+    >>> list(map(len, "{0:b}".format(5).split("0"))) 
+    '[1,1]'
+    #125(10) = 1111101(2)
+    >>> list(map(len, "{0:b}".format(125).split("0"))) 
+    '[5,1]'
+
+    * for print maximum number of consecutive 1's, most interesting ideas -
+    use 'sorted' method, and after reverse (reverse=True) this
+    new (sorted) list, get the first element in list [0].
+    This will be max value:
+    #125(10) = 1111101(2)
+    >>> sorted(list(map(len, '{0:b}'.format(125).split("0"))), reversed=True)[0]
+    '5'
+"""
 
 if __name__ == '__main__':
-
-    print(sorted(list(map(len,'{0:b}'.format(int(input().strip())).split("0"))),        
-    reverse=True)[0])
+    Num = int(input().strip())
+    print(sorted(list(map(len,
+                 '{0:b}'.format(Num).split("0"))),
+                 reverse=True)[0])
