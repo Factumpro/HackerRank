61046a95a7f94246ddb1915ccf0cbd49bf9a3056
Oleg <slidzonaoleg@gmail.com>
2021-03-27 12:45:50 +0100

    Update Day_04.py

--- Python/Tutorials/30 Days of Code/Day_04.py
+++ Python/Tutorials/30 Days of Code/Day_04.py
@@ -1,21 +1,21 @@
+#!/usr/bin/env python3
+
+
 class Person:
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge        
+    def __init__(self, initialAge):
         self.age = initialAge
-        if self.age < 0 : 
+        if self.age < 0:
             self.age = 0
             print('Age is not valid, setting age to 0.')
-        
+
     def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if self.age in range(13,18):
+        if self.age in range(13, 18):
             print('You are a teenager.')
         else:
             if self.age < 13:
-                print('You are young.')        
+                print('You are young.')
             else:
-                print('You are old.')             
-        
+                print('You are old.')
+
     def yearPasses(self):
-        # Increment the age of the person in here 
-        self.age += 1    
+        self.age += 1
