2474d382caec2a5a673b25e0c45638d6e9ee858e
Oleg <slidzonaoleg@gmail.com>
2021-03-27 12:42:01 +0100

    Update Day_03.py

--- Python/Tutorials/30 Days of Code/Day_03.py
+++ Python/Tutorials/30 Days of Code/Day_03.py
@@ -1,15 +1,7 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
+#!/usr/bin/env python3
 
 if __name__ == '__main__':
     n = int(input())
-    if n %2 == 0 and n not in range (6, 21):
+    if n % 2 == 0 and n not in range(6, 21):
         print ("Not", end=" "),
     print("Weird")
