73c071bafcbb13d6b456e84d9073887d8dc5054e
Oleg <slidzonaoleg@gmail.com>
2021-03-27 13:07:39 +0100

    Update Day_08.py

--- Python/Tutorials/30 Days of Code/Day_08.py
+++ Python/Tutorials/30 Days of Code/Day_08.py
@@ -1,15 +1,18 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
+#!/usr/bin/env python3
 
-names_phone_numbers = list(map(lambda _: input().split(), range(n)))
-phone_book = {book_name: book_phone for book_name,book_phone in names_phone_numbers}
-
-while True:
-    try:
-        name = input()
-        if name in phone_book:
-            print('{}={}'.format(name, phone_book[name]))
-        else:
-            print('Not found')
-    except:
-        break
+if __name__ == '__main__':
+    N = int(input())
+    names_phone_numbers = list(map(lambda _: input().split(), range(N)))
+    phone_book = {
+                  book_name: book_phone
+                  for book_name, book_phone in names_phone_numbers
+                  }
+    while True:
+        try:
+            name = input()
+            if name in phone_book:
+                print('{}={}'.format(name, phone_book[name]))
+            else:
+                print('Not found')
+        except:
+            break
