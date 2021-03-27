978adf7c17f745c55d488aa8f22ecaab08d453d5
Oleg <slidzonaoleg@gmail.com>
2021-03-27 12:47:09 +0100

    Update Day_05.py

--- Python/Tutorials/30 Days of Code/Day_05.py
+++ Python/Tutorials/30 Days of Code/Day_05.py
@@ -1,14 +1,6 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
+#!/usr/bin/env python3
 
 if __name__ == '__main__':
     n = int(input())
-    for i in range(10):        
-        print('{} x {} = {}'.format(n,i+1,n*(i+1)) )
+    for i in range(10):
+        print('{} x {} = {}'.format(n, i+1, n*(i+1)))
