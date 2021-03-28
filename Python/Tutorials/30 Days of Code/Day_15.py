777a1b8d8dbff1621093d953b4855054d487f092
Oleg <slidzonaoleg@gmail.com>
2021-03-28 08:56:03 +0200

    Update Day_15.py

--- Python/Tutorials/30 Days of Code/Day_15.py
+++ Python/Tutorials/30 Days of Code/Day_15.py
@@ -1,49 +1,50 @@
-'''
-Day 15: Linked List
-(FIFO)
+#!/usr/bin/env python3
 
-  Tutorial:
-  https://realpython.com/linked-lists-python/
+"""
+    Linked List (FIFO)
+    ------------------
 
-  linked lists have a performance advantage over normal lists when 
-  implementing a queue (FIFO), in which elements are continuously inserted 
-  and removed at the beginning of the list. But they perform similarly to 
-  a list when implementing a stack (LIFO), in which elements are inserted 
-  and removed at the end of the list.
-'''
+    Linked lists have a performance advantage over normal lists when
+    implementing a queue (FIFO), in which elements are continuously inserted
+    and removed at the beginning of the list. But they perform similarly to
+    a list when implementing a stack (LIFO), in which elements are inserted
+    and removed at the end of the list.
+
+    https://realpython.com/linked-lists-python/
+"""
+
 
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
-        self.next = None 
-class Solution: 
-    def display(self,head):
+        self.next = None
+
+
+class Solution:
+    def insert(self, head, data):
+        if head is None:
+            head = Node(data)
+            self.tail = head
+    # print(self.tail.data, self.tail.next, head.data, head.next,end=' - 0p\n')
+        else:
+            node = Node(data)
+    # print(self.tail.data, self.tail.next, head.data, head.next,end=' - 1p\n')
+            self.tail.next = node
+    # print(self.tail.data, self.tail.next, head.data, head.next,end=' - 2p\n')
+            self.tail = node
+    # print(self.tail.data, self.tail.next, head.data, head.next,end=' - 3p\n')
+        return head
+
+    def display(self, head):
         current = head
         while current:
             print(current.data, end=' ')
             current = current.next
 
-    def insert(self,head,data): 
-    #Complete this method 
-        if head is None:
-            head = Node(data)
-            self.tail = head
-            #print(self.tail.data, self.tail.next, head.data, head.next,end=' - 0p\n')
-        else: 
-            node = Node(data)
-            #print(self.tail.data, self.tail.next, head.data, head.next,end=' - 1p\n')
-            self.tail.next = node
-            #print(self.tail.data, self.tail.next, head.data, head.next,end=' - 2p\n')
-            self.tail = node
-            #print(self.tail.data, self.tail.next, head.data, head.next,end=' - 3p\n')
-        
-        return head
-
-
-mylist= Solution()
-T=int(input())
-head=None
-for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)    
-mylist.display(head); 	  
+if __name__ == '__main__':
+    my_list = Solution()
+    head = None
+    for _ in range(int(input())):
+        data = int(input())
+        head = my_list.insert(head, data)
+    my_list.display(head)
