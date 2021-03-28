94545e10efd3ad5c5c5be32e0a6cf23911485479
Oleg <slidzonaoleg@gmail.com>
2021-03-28 10:40:55 +0200

    Update Day_22.py

--- Python/Tutorials/30 Days of Code/Day_22.py
+++ Python/Tutorials/30 Days of Code/Day_22.py
@@ -1,47 +1,54 @@
-'''
-30/Day 22: Binary Search Trees
+#!/usr/bin/env python3
 
-  https://www.hackerrank.com/challenges/30-binary-search-trees/problem
-'''
+"""
+    Binary Search Trees
+    -------------------
+
+"""
+
 
 class Node:
-    def __init__(self,data):
-        self.right=self.left=None
+    def __init__(self, data):
+        self.right = None
+        self.left = None
         self.data = data
+
+
 class Solution:
-    def insert(self,root,data):
-        if root==None:
+    def insert(self, root, data):
+        if root is None:
             return Node(data)
         else:
-            if data<=root.data:
-                cur=self.insert(root.left,data)
-                root.left=cur
+            if data <= root.data:
+                current = self.insert(root.left, data)
+                root.left = current
             else:
-                cur=self.insert(root.right,data)
-                root.right=cur
+                current = self.insert(root.right, data)
+                root.right = current
+
         return root
 
-    def getHeight(self,root):
-        if root!=None: 
-            len_w_root = self._getHeight(root, 0)
-            len_wo_root = len_w_root -1
-            return len_wo_root
+    def get_height(self, root):
+        if root is not None:
+            height_w_root = self.get_height_(root, 0)
+            height_wo_root = height_w_root-1
+            return height_wo_root
         else:
             return 0
 
-    def _getHeight(self, root, cure_height):
-        if root == None: 
+    def get_height_(self, root, cure_height):
+        if root is None:
             return cure_height
-        left_height = self._getHeight(root.left, cure_height+1)
-        right_heigt = self._getHeight(root.right, cure_height+1)
-        return max(left_height, right_heigt)
-        
+        left_height = self.get_height_(root.left, cure_height+1)
+        right_height = self.get_height_(root.right, cure_height+1)
 
-T=int(input())
-myTree=Solution()
-root=None
-for i in range(T):
-    data=int(input())
-    root=myTree.insert(root,data)
-height=myTree.getHeight(root)
-print(height)       
+        return max(left_height, right_height)
+
+if __name__ == '__main__':
+    bin_tree = Solution()
+    root = None
+    for _ in range(int(input())):
+        data = int(input())
+        root = bin_tree.insert(root, data)
+    tree_height = bin_tree.get_height(root)
+    print(tree_height)
