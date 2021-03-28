54d65dd0dd88b181c75c1cdc0e67d2e5dafbf875
Oleg <slidzonaoleg@gmail.com>
2021-03-28 09:32:57 +0200

    Update Day_19.py

--- Python/Tutorials/30 Days of Code/Day_19.py
+++ Python/Tutorials/30 Days of Code/Day_19.py
@@ -1,36 +1,28 @@
-'''
-Day 19: Interfaces
+#!/usr/bin/env python3
 
-'''
+import math
+
 
 class AdvancedArithmetic(object):
     def divisorSum(n):
         raise NotImplementedError
 
-import math
+
 class Calculator(AdvancedArithmetic):
-    def divisorSum(self, n):
-        _sumd = 0
-        i = 1
-        '''
-        for i in range(1,n+1):
-            if (n % i==0) :
-                _sumd +=i
-            i = i + 1
-        return _sumd
-        '''
-        for i in range(1,int(math.sqrt(n))+1):        
-            if (n % i == 0) :  
-                if (n / i == i) :
-                    _sumd +=i
-                else :                
-                    _sumd +=i + n/i
-            i = i + 1
-        return int(_sumd)
-        
-        
-n = int(input())
-my_calculator = Calculator()
-s = my_calculator.divisorSum(n)
-print("I implemented: " + type(my_calculator).__bases__[0].__name__)
-print(s)
+    def divisor_sum(self, n):
+        div_sum_ = 0
+        for i in range(1, int(math.sqrt(n))+1):
+            if n % i == 0:
+                if n / i == i:
+                    div_sum_ += i
+                else:
+                    div_sum_ += i + n/i
+            i += 1
+        return int(div_sum_)
+
+if __name__ == '__main__':
+    n = int(input())
+    my_calc = Calculator()
+    s = my_calc.divisorSum(n)
+    print("I implemented: ", type(my_calc).__bases__[0].__name__)
+    print(s)
