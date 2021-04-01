6cd173894897b14d599f66d5499dcf93095c7389
git__repo[login] <git__repo[email]>
2021-04-01 06:43:00 +0000

    ./Warmup/. solved

---
+++ Python/Algorithms/Warmup/solve_me_first.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def solveMeFirst(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    num1 = int(input())
+    num2 = int(input())
+    res = solveMeFirst(num1, num2)
+    print(res)
