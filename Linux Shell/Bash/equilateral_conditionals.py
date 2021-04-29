c016ff5e4cba032718c173bf48d4e84d200a975e
Factumpro <slidzonaoleg@gmail.com>
2021-04-29 06:12:37 +0000

    add Bash/More on Conditionals solved

---
+++ Linux Shell/Bash/equilateral_conditionals.py
@@ -0,0 +1,13 @@
+#!/usr/bin
+read x
+read y
+read z
+if [[ $x == $y && $y == $z ]]
+then
+    echo 'EQUILATERAL'
+elif [[ $x == $y || $y == $z || $x == $z ]]
+then
+    echo 'ISOSCELES'
+else
+    echo 'SCALENE'
+fi
